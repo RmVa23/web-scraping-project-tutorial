694d23cf4459fd7701f9ea117752904bbaf10d79
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-03 19:26:26 +0000

    WebScrapping con Selenium

---
+++ src/WebScrapping-local.py
@@ -0,0 +1,95 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import pandas as pd
+import time
+
+# Configuramos el driver de Selenium
+driver = webdriver.Firefox()
+
+url ='https://coinmarketcap.com/es/currencies/ethereum/historical-data/'
+
+# Cargamos la página con Selenium
+driver.get(url)
+
+# Esperamos a que la página cargue completamente (se puede ajustar el tiempo según sea necesario)
+driver.implicitly_wait(1000)  # Esperamos hasta 10 segundos
+
+time.sleep(10) # hacemos que espere 10 segundos para que compile bien los datos (hubo problemas al ejecutar el codigo poor el tiepo de carga)
+
+# Obtenemos el HTML de la página después de que se haya cargado completamente
+html = driver.page_source
+
+# Cierramos el driver de Selenium
+driver.quit()
+
+# Parseamos el HTML con BeautifulSoup
+soup = BeautifulSoup(html, 'html.parser')
+
+tabla = soup.find('table', {'class':"sc-14cb040a-3 eGIvUX cmc-table"})
+
+datos = []
+
+if tabla:
+    elementos = tabla.find_all('tr')
+    for elemento in elementos:
+        celdas = elemento.find_all('td')
+        if len(celdas) == 7:
+            fecha = celdas[0].text.strip()
+            abrir = celdas[1].text.strip()
+            alto = celdas[2].text.strip()
+            bajo = celdas[3].text.strip()
+            cerrar = celdas[4].text.strip()
+            volumen = celdas[5].text.strip()
+            cap_mercado = celdas[6].text.strip()
+
+            fila = {
+                'Fecha': fecha,
+                'Abrir': abrir,
+                'Alto': alto,
+                'Bajo': bajo,
+                'Cerrar': cerrar,
+                'Volumen': volumen,
+                'Cap. de Mercado': cap_mercado}
+
+            datos.append(fila)
+
+data = pd.DataFrame(datos)
+print(data)
+
+# Obtenemos un dataframe con los datos de ETH del último trimestre.
+# Para obtener otro período, tendríamos que recurrir a la API del sitio,
+# lo que haremos en el proyecto API, para obtener los datos mensuales de los últimos años
+
+data.to_csv('1-trimestre-2024-ETH.csv', index=False)
+
+# ------------------------Fin del código de obtención de datos---------------------------------------------------
+
+# He probado sin resultado favorable las siguientes soluciones para ejecutar este código desde el codespace:
+# - 1. En el Jupyter Notebook use HtmlUnitDriver: Navegador sin cabeza que está integrado directamente en Selenium WebDriver y no requiere la instalación de controladores externos.
+# - 2. Tambien descargue del driver de Firefox (geckodriver) y probe distintas formas, como las siguientes
+
+# from selenium.webdriver import Firefox
+# from selenium.webdriver.firefox.options import Options
+
+# gecko_driver = r'C:\Users\rmace\Gecko Driver - Webdriver Firefox\geckodriver-v0.34.0-win-aarch64\geckodriver.exe'
+
+# firefox_options = Options()
+# firefox_options.headless = True 
+
+# driver = Firefox(options=firefox_options, executable_path=gecko_driver)
+#---------------------------------------------------------------
+
+# from selenium import webdriver
+
+# gecko_driver = r'C:\Users\rmace\Gecko Driver - Webdriver Firefox\geckodriver-v0.34.0-win-aarch64\geckodriver.exe'
+
+# firefox_options = webdriver.FirefoxOptions()
+# firefox_options.headless = True  
+
+# driver = webdriver.Firefox(executable_path=gecko_driver, options=firefox_options)
+
+# También consideré usar Selenium Grid (para ejecutar pruebas automatizadas en una máquina remota
+# sin necesidad de descargar e instalar un controlador específico en tu entorno local). 
+# También Sauce Labs (servicio en la nube que ofrece navegadores y ambientes preconfigurados para pruebas automatizadas), y proporciona una API.
+# Seguiré trabajando en encontrar una solución que me permita ejecutar el código, tanto si es una de las mencionadas anteriormente,
+# como si es la descarga de otro driver como el de Chrome.
--- src/app.py
+++ src/app.py
@@ -1,15 +1,23 @@
-import os
-from sqlalchemy import create_engine
+import requests
+from bs4 import BeautifulSoup
+import re
 import pandas as pd
-from dotenv import load_dotenv
+import urllib.request
 
 # load the .env file variables
-load_dotenv()
+#load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+# 1) Connect to the database here using the SQLAlchemy's create_engine functio
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+def scrape_Web(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise Exception(f'Error al acceder a la URL: {url, response.status_code}')
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+print(scrape_Web(url))
